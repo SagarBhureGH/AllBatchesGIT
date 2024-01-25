e5c4e23984856a2dbfaa241a7a9df85618858eff
sagar <sagar.bhure.756@gmail.com>
2024-01-25 14:59:01 +0530

    modify code 2

---
+++ GitDemoProject/GitDemo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemoProject
+{
+    internal class GitDemo
+    {
+        public GitDemo() { 
+        
+        }
+
+        public int Demo() { 
+
+        return 0;
+        }
+    }
+}
